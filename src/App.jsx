19ef3bc7137428198168d40ebdc8159dcb22915f
= <114255759+rocmal1@users.noreply.github.com>
2024-04-12 22:07:25 -0700

    Add React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <>
+      <h1>Hi from React!</h1>
+    </>
+  );
+}
+
+export default App;
--- src/renderer.js
+++ src/renderer.js
@@ -27,7 +27,7 @@
  */
 
 import "./index.css";
-import "./app.jsx";
+import "./index.jsx";
 
 console.log(
   '👋 This message is being logged by "renderer.js", included via webpack'
