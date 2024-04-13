6561b6a53d12f28778ebbb1f3c87a85a23e6dab1
= <114255759+rocmal1@users.noreply.github.com>
2024-04-12 22:23:37 -0700

    Add program phases

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
+  const Phases = {
+    INITIAL: 0,
+    LISTEN_TO_TARGET: 1,
+    LISTEN_WITH_TRANSLATION: 2,
+    READ_TARGET_ALOUD_WITH_TRANSLATION: 3,
+    READ_TARGET_ALOUD_WITHOUT_TRANSLATION: 4,
+    LISTEN_AND_READ_BASE: 5,
+    LISTEN_AND_READ_TARGET: 6,
+    LISTEN_TO_TARGET_2: 7,
+    LISTEN_AND_REPEAT: 8,
+    READ_COMMENTS_AND_EXAMINE_SENTENCES: 9,
+    COMPLETE_EXERCISES: 10,
+  };
+  const [phase, setPhase] = useState(Phases.INITIAL);
+
   return (
     <>
       <h1>Hi from React!</h1>
